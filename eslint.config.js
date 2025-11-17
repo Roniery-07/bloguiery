05f33482059952dccda4f4f733c69f65fc4894cb
Roniery Abreu <heroniery007@gmail.com>
2025-11-17 11:59:45 -0300

    implements my own eslint + prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,4 @@
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
-import prettierConfig from "eslint-plugin-prettier/recommended";
-import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+import ronieryEslintConfig from 'roniery-eslint-config/react.js';
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  ...tseslint.configs.recommended,
-
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-  prettierConfig,
-]);
+export default defineConfig([...ronieryEslintConfig]);

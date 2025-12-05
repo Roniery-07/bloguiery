d702ac337f18ba3a1ae99a09590680f99c9ad271
Roniery-07 <heroniery007@gmail.com>
2025-12-04 21:13:46 -0300

    implements basic about page, must implement more things

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -4,7 +4,8 @@ export const Home = () => {
   return (
     <div className="page">
       <div className="w-full p-4 md:max-w-5xl grid grid-cols-1 md:grid-cols-3 min-h-full gap-4">
-        <div className="col-span-1 card-retro flex flex-col gap-4">
+        <div className="col-span-1 md:col-span-2 card-retro flex flex-col gap-4 justify-center">
+          <h1 className="text-xl font-bold uppercase">Title</h1>
           <span>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
             omnis eos necessitatibus aut magnam, iusto quisquam odit cupiditate,
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -5,12 +5,13 @@ import {
   Route,
 } from 'react-router-dom';
 import { Home } from './Home';
+import { About } from './About';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<Home />} />
-      <Route path="about" element={<div>About page</div>} />
+      <Route path="about" element={<About />} />
       <Route path="posts" element={<div>Posts page</div>} />
       <Route path="contact" element={<div>Contact page</div>} />
     </Route>,
